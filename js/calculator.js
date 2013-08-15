870b903622f35dc1cedd7c9d6c15402ea3e8e4d6
Sisi Wei <me@sisiwei.com>
2013-08-14 23:29:35 -0400

    Moving jquery template script to js file so we can reverse instructions to say delete

--- js/calculator.js
+++ js/calculator.js
@@ -61,7 +61,7 @@ $(document).ready(function(){
         try {
             $('#calculator-preview').html(calculatorSource);
             // And source to copy-paste
-            $('#calculatorSource').text(calculatorSource);
+            $('#calculatorSource').val('        <!-- Delete the next line if you do not already have jQuery on your page: -->\n        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>' + calculatorSource);
             $('.reveal').fadeIn(500);
             $('.error').fadeOut();
         }
