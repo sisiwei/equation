13867f1eaf8444c3371f16d979bb5cba9a2a25c8
Steven Melendez <smelendez@wnyc.org>
2013-08-23 14:07:46 -0400

    Scroll fix

--- js/calculator.js
+++ js/calculator.js
@@ -69,9 +69,6 @@ $(document).ready(function(){
             // And source to copy-paste
             $('#calculatorSource').val('\n        <!-- Delete the next line if you already have jQuery on your page: -->\n        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>' + calculatorSource);
             $('.reveal').fadeIn(500);
-            $(window).animate({
-                scrollTop: $('.try').position().top
-            },500, 'easeOutExpo');
             $('.error').fadeOut();
         }
         catch (e) {
@@ -82,6 +79,9 @@ $(document).ready(function(){
             $('.error').fadeIn(100);
             setTimeout(function(){ $('.error').fadeOut(); }, 1500)
         }
+        $('body').animate({
+            scrollTop: $('.try').position().top
+        },500);
 
     }
     $('#generateButton').click(generate);
